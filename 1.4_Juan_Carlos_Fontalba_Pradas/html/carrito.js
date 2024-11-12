331e67ba842aa9ce8865d3e2f4432e8d94867a67
Aster <127297209+AsterJL@users.noreply.github.com>
2024-11-12 10:09:22 +0100

    Add files via upload

---
+++ 1.4_Juan_Carlos_Fontalba_Pradas/html/carrito.js
@@ -0,0 +1,4 @@
+function toggleCartDropdown() {
+    const cartDropdown = document.getElementById('cart-dropdown');
+    cartDropdown.style.display = cartDropdown.style.display === 'block' ? 'none' : 'block';
+}
